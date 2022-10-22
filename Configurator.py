3e0df0bd09d8881b8fb1e86be85af1eef2ba3dc5
MateuszMalowiecki <33604498+MateuszMalowiecki@users.noreply.github.com>
2022-10-22 21:01:29 +0200

    Added project files

---
+++ Configurator.py
@@ -0,0 +1,36 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.button import Button
+from kivy.uix.spinner import Spinner
+
+class ConfigurationWindow(Screen):
+    size_selection = ObjectProperty(None)
+    base_selection = ObjectProperty(None)
+    price_label = ObjectProperty(None)
+    have_cup = False
+    def count_prize(self):
+        price = 16
+        if self.size_selection.text == "Big":
+            price += 4
+        if self.base_selection.text == "With milk":
+            price += 2
+        if self.have_cup:
+            price *= 0.9
+        self.price_label.text = f"Price: {price}"
+    def change_info_about_cap(self, instance, value):
+       self.have_cup = value
+
+class WindowManager(ScreenManager):
+    pass
+
+kv = Builder.load_file("Configurator.kv")
+
+class ConfiguratorApp(App):
+    def build(self):
+        return kv
+
+if __name__ == "__main__":
+    ConfiguratorApp().run()
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Bubble-tea-configurator
-Simple configurator of bubble tea written in Python
+Simple configurator of bubble tea written in Pythonk
+
+# Launching the project:
+First you need to install Python. Then you need to install Kivy with the command:
+```
+pip install kivy
+```
+After installing Kivy you can start the application by running the command:
+```
+python Configurator.py
